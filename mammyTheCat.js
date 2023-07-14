e524a9a2c1d67a406ea235987632ac7cebc3f591
PreslavaN99 <139551714+PreslavaN99@users.noreply.github.com>
2023-07-14 13:51:00 +0300

    Add files via upload

---
+++ mammyTheCat.js
@@ -0,0 +1,11 @@
+function mammyTheCat (input){
+    let doYouLoveMe = input [0];
+
+    if (doYouLoveMe === "YES!") {
+        console.log ("Correct answer. I love You! <3");
+    } else {
+        console.log ("Wrond answer!!!");
+    }
+}
+
+mammyTheCat (["YES!"]);
